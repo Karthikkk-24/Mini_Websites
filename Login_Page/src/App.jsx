c3c8ae8c50b7627c96baa0daa9d6317e66fe45bb
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2022-12-08 22:08:27 +0530

    Final Update

--- Login_Page/src/App.jsx
+++ Login_Page/src/App.jsx
@@ -1,12 +1,8 @@
 import { useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 import Form from "./components/Form";
-import FormComponents from "./components/FormComponents";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <div className="body">
